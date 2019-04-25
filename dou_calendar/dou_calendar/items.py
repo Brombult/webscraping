bb8675c25272b866b7b5fa3d336309d95ad6cab9
Boris R <rickrolled5997@gmail.com>
2019-04-25 13:24:49 +0300

    added custom item loader for cleaning up scraped data

--- dou_calendar/dou_calendar/items.py
+++ dou_calendar/dou_calendar/items.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from scrapy.loader import ItemLoader
+
+
+def strip_value_processor(self, values):
+    """This processor removes spaces and newlines from the beginning and the end of the string"""
+    for value in values:
+        yield value.strip()
 
 
 class DouCalendarItem(scrapy.Item):
@@ -15,3 +16,13 @@ class DouCalendarItem(scrapy.Item):
     Price = scrapy.Field()
     Where = scrapy.Field()
     Link = scrapy.Field()
+
+
+class CalendarItemLoader(ItemLoader):
+    """Custom ItemLoader for cleaning up the data"""
+    Title_in = strip_value_processor  # "_in" stands for "input"
+    Description_in = strip_value_processor
+    Date_in = strip_value_processor
+    Price_in = strip_value_processor
+    Where_in = strip_value_processor
+    Link_in = strip_value_processor
--- dou_calendar/dou_calendar/spiders/events.py
+++ dou_calendar/dou_calendar/spiders/events.py
@@ -4,9 +4,8 @@ Spider that scrapes Dou Calendar events
 Can be run with "scrapy crawl events -o events.csv" command in terminal
 """
 import scrapy
-from scrapy.loader import ItemLoader
 
-from dou_calendar.items import DouCalendarItem
+from dou_calendar.items import DouCalendarItem, CalendarItemLoader
 
 
 class EventsSpider(scrapy.Spider):
@@ -15,17 +14,17 @@ class EventsSpider(scrapy.Spider):
     start_urls = [f'https://dou.ua/calendar/page-{num}/' for num in range(1, 17)]
 
     def parse(self, response):
-        l = ItemLoader(item=DouCalendarItem(), response=response)
+        l = CalendarItemLoader(item=DouCalendarItem(), response=response)
         events = response.css('article')
         for event in events:
-            title = event.css('h2.title > a::text').extract_first().strip()
-            desc = event.css('p.b-typo::text').extract_first().strip()
-            date = event.css('div.when-and-where > .date::text').extract_first().strip()
+            title = event.css('h2.title > a::text').extract_first()
+            desc = event.css('p.b-typo::text').extract_first()
+            date = event.css('div.when-and-where > .date::text').extract_first()
             try:
-                price = event.css('div.when-and-where > .date + span::text').extract_first().strip()
+                price = event.css('div.when-and-where > .date + span::text').extract_first()
             except AttributeError:
                 price = 'None'
-            where = event.xpath('//div[@class="when-and-where"]').extract_first().split()[5].strip()
+            where = event.xpath('//div[@class="when-and-where"]').extract_first().split()[5]
             link = event.xpath('.//h2[@class="title"]/a/@href').extract_first()
 
             l.add_value('Title', title)
