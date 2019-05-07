00775de148a395eb838231ea176a8fa4bc02aa68
Boris R <rickrolled5997@gmail.com>
2019-05-07 11:39:42 +0300

    refactored for better maintainability and readability

--- dou_scraping/dou_selenium_scraping.py
+++ dou_scraping/dou_selenium_scraping.py
@@ -8,82 +8,111 @@ from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import ElementNotVisibleException, WebDriverException
 from pandas import DataFrame
 
+JOBS_DOU_URL = 'https://jobs.dou.ua/vacancies/'
+BEGINNERS_CATEGORY_LIST = ['Начинающим', 'Початківцям', 'For beginners']
+RELOCATION_CATEGORY_LIST = ['За рубежом', 'За кордоном', 'Relocation']
+CSV_COLUMNS_NAME = ['name', 'company', 'info', 'link']
 
-# TODO: consider adding DB support and turning this into telegram bot
+CATEGORY_NAME = "QA"
+CITY = "Kiev"
 
-def scrape_dou_vacancies(city, category):
+
+def init_headless_driver():
     """
-    Scrapes info about job openings, then writes that info into a csv file
-    Requires two arguments: city and job category
+    Initiating and configuring headless browser
+    Returns webdriver instance
     """
-    csv_file_name = f'{city}_{category}_{datetime.date.today()}.csv'
-    csv_columns = ['name', 'company', 'info', 'link']
-    jobs_dou_url = 'https://jobs.dou.ua/vacancies/'
-    beginners_category_list = ['Начинающим', 'Початківцям', 'For beginners']
-    relocation_category_list = ['За рубежом', 'За кордоном', 'Relocation']
-
     options = Options()
     options.headless = True
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(5)
     print('Headless browser is initiated')
 
-    if category in beginners_category_list:
-        driver.get(f'{jobs_dou_url}?city={city}&beginners')
-    elif category in relocation_category_list:
-        driver.get(f'{jobs_dou_url}?relocation')
+    return driver
+
+
+def open_dou_vacancies(driver, category, city):
+    """
+    Handles user input and opens jobs.dou
+    :param driver: webdriver instance
+    :param category: job category (e.g. 'QA' or 'Python')
+    :param city: city to search vacancies in
+    :return: webdriver instance and csv file name, constructed with category and city names
+    """
+    csv_file_name = f'{city}_{category}_{datetime.date.today()}.csv'
+
+    if category in BEGINNERS_CATEGORY_LIST:
+        driver.get(f'{JOBS_DOU_URL}?city={city}&beginners')
+    elif category in RELOCATION_CATEGORY_LIST:
+        driver.get(f'{JOBS_DOU_URL}?relocation')
     else:
-        driver.get(f'{jobs_dou_url}?city={city}&category={category}')
+        driver.get(f'{JOBS_DOU_URL}?city={city}&category={category}')
     print('Jobs.dou is open')
 
-    def click_on_more_jobs_button():
-        """
-        Since there's no pagination on jobs.dou this function clicks through all "more jobs" buttons
-        so every job opening is present on the page and can be scraped
-        """
-        try:
-            more_vacancies_button = driver.find_element_by_css_selector('.more-btn > a')
-            while more_vacancies_button:
-                more_vacancies_button.click()
-        except ElementNotVisibleException:
-            print("Scraping has started\n")
-        except WebDriverException:
-            driver.quit()
-            sys.exit('Webdriver bugged out, please run the script again')
+    return driver, csv_file_name
 
-    def get_vacancy_info():
-        """
-        Gets every job openings on the page, returns list of dictionaries with collected info about those job openings
-        """
-        click_on_more_jobs_button()
-        all_vacancies = []
-        vacancies_counter = 0
 
-        soup = BeautifulSoup(driver.page_source, 'lxml')
+def click_on_more_jobs_button(driver):
+    """
+    Since there's no pagination on jobs.dou this function clicks through all "more jobs" buttons
+    so every job opening is present on the page and can be scraped.
+    Returns webdriver instance
+    """
+    try:
+        more_vacancies_button = driver.find_element_by_css_selector('.more-btn > a')
+        while more_vacancies_button:
+            more_vacancies_button.click()
+    except ElementNotVisibleException:
+        print("Scraping has started\n")
+    except WebDriverException:
+        driver.quit()
+        sys.exit('Webdriver bugged out, please run the script again')
 
-        try:
-            vacancies = soup.find_all(name='li', class_='l-vacancy')
-            for vacancy in vacancies:
-                title = vacancy.find(name='div', class_='title')
-                name = title.find(name='a', class_='vt')
-                company = title.find(name='a', class_='company')
-                info = vacancy.find(name='div', class_='sh-info')
-                link = name.attrs['href']
+    return driver
 
-                vacancy_details_dict = {
-                    'name': name.text, 'company': company.text, 'info': info.text.strip(), 'link': link}
-                all_vacancies.append(vacancy_details_dict)
-                vacancies_counter += 1
-                print(f'Vacancies scraped: {vacancies_counter}')
 
-            return all_vacancies
-        except Exception as exc:
-            print(exc)
-        finally:
-            driver.quit()
+def get_vacancy_info(driver):
+    """
+    Gets every job openings on the page.
+    Returns list of dictionaries with collected info those job openings
+    """
+    click_on_more_jobs_button(driver)
+    all_vacancies = []
+    vacancies_counter = 0
 
-    # saving vacancies info to csv file
-    data = get_vacancy_info()
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+
+    try:
+        vacancies = soup.find_all(name='li', class_='l-vacancy')
+        for vacancy in vacancies:
+            title = vacancy.find(name='div', class_='title')
+            name = title.find(name='a', class_='vt')
+            company = title.find(name='a', class_='company')
+            info = vacancy.find(name='div', class_='sh-info')
+            link = name.attrs['href']
+
+            vacancy_details_dict = {
+                'name': name.text, 'company': company.text, 'info': info.text.strip(), 'link': link}
+            all_vacancies.append(vacancy_details_dict)
+            vacancies_counter += 1
+            print(f'Vacancies scraped: {vacancies_counter}')
+
+        return all_vacancies
+    except Exception as exc:
+        print(exc)
+    finally:
+        driver.quit()
+
+    return all_vacancies
+
+
+def save_data_to_csv(data, csv_file_name, csv_columns):
+    """
+    Saves data to csv file
+    :param data: data to save
+    :param csv_file_name: csv file name
+    :param csv_columns: csv columns
+    """
     data_frame = DataFrame(data, columns=csv_columns)
     try:
         data_frame.to_csv(csv_file_name)
@@ -94,4 +123,6 @@ def scrape_dou_vacancies(city, category):
 
 
 if __name__ == '__main__':
-    scrape_dou_vacancies('Київ', 'QA')
+    driver, file_name = open_dou_vacancies(init_headless_driver(), category=CATEGORY_NAME, city=CITY)
+    vacancies = get_vacancy_info(driver)
+    save_data_to_csv(vacancies, file_name, CSV_COLUMNS_NAME)
